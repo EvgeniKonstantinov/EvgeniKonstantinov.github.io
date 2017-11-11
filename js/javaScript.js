b64a0df9db2e6c57a701a825660046f9a8f45d92
EvgeniKonstantinov <nenyznoe.mbllo@gmail.com>
2017-11-11 23:47:58 +0300

    update

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,6 +1,17 @@
-$(window).resize(function() {
-    width = $(window).width();
-    if (width >= 600) {
+
+// делаете переменную в начале кода
+var isMobile = false;
+// проверка на размер экрана (размер я брал вроде с Bootstrap-а)
+$(document).ready( function() {
+    if ($('body').width() <= 400) {
+        isMobile = true;
+    }
+    // и потом если нужен код только для телефона:
+    if (isMobile) {
+    
+    }
+    // или для остальных
+    if (!isMobile) {
         $(window).scroll(function () {
             var st = $(this).scrollTop();
             var bo = $(this).scrollTop();
@@ -31,7 +42,7 @@ $(window).resize(function() {
             });
         });
     }
-});
+} );
 
 
 
