68325cb00d10e3b715a6bf64b1a89b8c546ac6cb
evgenikonstantinov <evgeniikonstantinov@gmail.com>
2017-11-22 15:18:41 +0300

    new ver

---
+++ wordpress/js/common.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    function heightDetect() {
+        $(".main_head").css('height', $(window).height());
+    }
+    heightDetect();
+	$(window).resize(function () {
+        heightDetect();
+    })
+});
+
+$(window).load(function() {
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
+});
+
+$('.toggle_menu').click(function () {
+    if ($('.top_menu').is(":visible")){
+        $('.top_menu').fadeOut(600);
+        $('.top_menu li a').removeClass("fadeInUp animated");
+    } else{
+        $('.top_menu').fadeIn(600);
+        $('.top_menu li a').addClass("fadeInUp animated");
+    }
+});
+
+$(".toggle_menu").click(function() {
+    $(".sandwich").toggleClass("active");
+}
+);
+
+$('.top_menu ul a').click(function () {
+    $('.top_menu').fadeOut(600);
+    $(".sandwich").toggleClass("active");
+
+})
